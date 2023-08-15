06a5405715d7b69d48dc640ddbae20cbc8c12895
gitkbhagat <142294807+gitkbhagat@users.noreply.github.com>
2023-08-15 12:44:02 +0530

    Update streamlit_app.py
    added avacado toast and emojis

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,7 @@ import streamlit
 streamlit.title('My parents New Healthy Diner')
 
 streamlit.header('Breakfast Menu')
-streamlit.text('Omega3 and Blueberry oatmeal')
-streamlit.text('Kale, Spinach and Rocket Smoothie')
-streamlit.text('Hard-Boiled Free_Ranged Egg')
+streamlit.text('🥣 Omega3 and Blueberry oatmeal')
+streamlit.text('🥗 Kale, Spinach and Rocket Smoothie')
+streamlit.text('🐔 Hard-Boiled Free_Ranged Egg')
+streamlit.text('🥑🍞 Avacado Toast')
