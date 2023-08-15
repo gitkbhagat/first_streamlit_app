75a7436c80c5b93a6d11ce9c7eb1c6b87a87581d
gitkbhagat <142294807+gitkbhagat@users.noreply.github.com>
2023-08-15 13:37:49 +0530

    Update streamlit_app.py
    corrected typo in starwberries

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,7 +15,7 @@ my_fruit_list=pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/
 my_fruit_list = my_fruit_list.set_index('Fruit')
 
 # Let's put a pick list here so they can pick the fruit they want to include 
-Fruits_selected=streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avacado','Starwberries'])
+Fruits_selected=streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avacado','Strawberries'])
 Fruits_to_show=my_fruit_list.loc[Fruits_selected]
 
 #to display table on the page
